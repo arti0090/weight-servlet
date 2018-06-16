de0150ed6ced9ebfe76df33a6ae212d3bb4d27fa
arti0090 <arti0090@gmail.com>
2018-06-16 10:55:21 +0200

    Hello Weight
    Modified files so servlet is poping with first message

--- src/main/java/logger/weight/controller/WeightServlet.java
+++ src/main/java/logger/weight/controller/WeightServlet.java
@@ -1,4 +1,68 @@
 package logger.weight.controller;
 
-public class WeightServlet {
+import logger.weight.service.WeightService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "weight-servlet", urlPatterns = {"/",})
+public class WeightServlet extends HttpServlet{
+
+    public static final String TEXT_HTML = "text/html";
+
+
+
+
+
+
+
+    private WeightService weightService;
+
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
+        response.setContentType(TEXT_HTML);
+
+
+        request.getRequestDispatcher("hello.jsp").forward(request, response);
+    }
+
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("Servlet " + this.getServletName() + " has started");
+    }
+    @Override
+    public void destroy() {
+        System.out.println("Servlet " + this.getServletName() + " has stopped");
+    }
+
 }
+
+
+
+
